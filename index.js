0bb9fd129b9a2ec9efbc994c5a4abaf054c2e4c2
steven <stevenlaqu@gmail.com>
2021-06-25 19:09:55 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+export { MyComponent } from './src/MyComponent.js';
---
+++ my-component.js
@@ -0,0 +1,3 @@
+import { MyComponent } from './src/MyComponent.js';
+
+window.customElements.define('my-component', MyComponent);
